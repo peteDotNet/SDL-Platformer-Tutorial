9dabeab993f7f225d98fb26b2a85697422a57b96
Peter Morton <petermorton@live.co.uk>
2020-10-07 14:40:26 +0100

    Add project files.

---
+++ SDL/main.cpp
@@ -0,0 +1,48 @@
+#include <SDL.h>
+#include <iostream>
+#include <SDL_image.h>
+#include "RenderWindow.hpp"
+#include "Entity.hpp"
+
+int main(int argc, char* argv[])
+{
+	if (SDL_Init(SDL_INIT_VIDEO) > 0)
+		std::cout << "SDL_Init has failed: " << SDL_GetError() << std::endl;
+
+	if (!IMG_Init(IMG_INIT_PNG))
+		std::cout << "IMG_init has failed: " << SDL_GetError() << std::endl;
+
+	RenderWindow window("Game v1.0", 1280, 720);
+
+	SDL_Texture* grassTexture = window.loadTexture("ground_grass_1.png");
+
+	Entity entities[3] = { Entity(0,0,grassTexture),
+	Entity(0,100,grassTexture),
+	Entity(100,0,grassTexture) };
+
+	bool gameRunning = true;
+
+	SDL_Event event;
+
+	while (gameRunning) {
+		while (SDL_PollEvent(&event))
+		{
+			if (event.type == SDL_QUIT)
+				gameRunning = false;
+		}
+		window.clear();
+
+		for (int i = 0; i < 3; i++) {
+
+			window.render(entities[i]);
+		}
+		window.display();
+	}
+
+	window.cleanUp();
+
+
+	SDL_Quit();
+
+	return 0;
+}
