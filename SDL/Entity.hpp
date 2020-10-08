efc814fd03256272b10e7433928a97d5e6720d38
Peter Morton <petermorton@live.co.uk>
2020-10-08 16:23:41 +0100

    Repurposed game loop from tutorial to create snake game.

--- SDL/Entity.hpp
+++ SDL/Entity.hpp
@@ -7,15 +7,32 @@
 class Entity {
 public:
 	Entity(Vector2f p_pos, SDL_Texture* p_tex);
+	
+
+
 	Vector2f& getPos()
 	{
 		return pos;
 	}
+
+	void SetPos(Vector2f p_pos){
+		pos = p_pos;
+	}
+
+	void Move();
+
+	void Input();
 	
 	SDL_Texture* getTex();
 	SDL_Rect getCurrentFrame();
+
+
+
 private:
+	enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+	eDirection dir;
 	Vector2f pos;
-	SDL_Rect currentFrame; 
+	Vector2f nextPos;
+	SDL_Rect currentFrame;
 	SDL_Texture* tex;
 };
--- SDL/Utils.hpp
+++ SDL/Utils.hpp
@@ -7,4 +7,49 @@ namespace utils {
 		t *= 0.001f;
 		return t;
 	}
+
+
+	void checkForClashWithFood(Entity head, Entity& food, std::vector<Entity>& tailEntities, SDL_Texture* p_tex) {
+
+		Vector2f headPos = head.getPos();
+		Vector2f foodPos = food.getPos();
+
+		float deltaX = abs(headPos.x - foodPos.x);
+		float deltaY = abs(headPos.y - foodPos.y);
+
+		if (deltaX < 32 && deltaY < 32) {
+
+
+			Vector2f pos = Vector2f(rand() % 500, rand() % 500);
+			food.SetPos(pos);
+
+			for (int i = 0; i < 5; i++) {
+				Entity bodySegment = Entity(headPos, p_tex);
+				tailEntities.push_back(bodySegment);
+			}
+		}
+	}
+
+	bool checkForClashWithWalls(Entity head) {
+		Vector2f headPos = head.getPos();
+		if (headPos.x > 500 || headPos.y > 500 || headPos.x < 0 || headPos.y < 0)
+			return false;
+		return true;
+	}
+
+
+	void initBody(Entity head, std::vector<Entity>& tailEntities) {
+
+		Vector2f nextPos = head.getPos();;
+		for (Entity& e : tailEntities)
+		{
+			Vector2f currentPos = e.getPos();
+			e.SetPos(nextPos);
+			nextPos = currentPos;
+		}
+
+	}
+
+
+
 }
